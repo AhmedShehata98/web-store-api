92866c3c3842bb7386df94236ba910efcafbed7d
Ahmed Shehata <ahmedshehataq98@gmail.com>
2024-01-31 19:12:22 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,9 @@ import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
-  const isDevelopment = process.env.NODE_ENV === 'development';
-
   const app = await NestFactory.create(AppModule, {
     cors: {
-      origin: isDevelopment
-        ? 'http://localhost:3000'
-        : 'https://web-store-apps.vercel.app',
+      origin: process.env.ORIGIN,
       credentials: true,
     },
     bodyParser: true,
@@ -18,7 +14,7 @@ async function bootstrap() {
 
   app.setGlobalPrefix('api');
   app.use(cookieParser());
-  app.enableCors({});
+
   await app.listen(process.env.PORT || 7000);
 }
 bootstrap();
