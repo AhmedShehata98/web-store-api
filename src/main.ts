8614a335e23e6aac2a087755fd43269bc6892e25
Ahmed Shehata <ahmedshehataq98@gmail.com>
2024-02-10 14:58:12 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import * as cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     cors: {
-      origin: '*',
+      origin: process.env.ORIGIN,
       credentials: true,
     },
     bodyParser: true,
